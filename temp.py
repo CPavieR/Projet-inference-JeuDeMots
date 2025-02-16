8bd3fc32aa649343fdf790b6f894bb368c50dd6a
CPavieR <mag@cleparo.fr>
2025-02-16 12:15:32 +0100

    add graph plotting

--- temp.py
+++ temp.py
@@ -1,11 +1,13 @@
 # https://jdm-api.demo.lirmm.fr/schema
 import requests
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
 link_api ="https://jdm-api.demo.lirmm.fr/schema"
 api_get_node_by_name = "https://jdm-api.demo.lirmm.fr/v0/node_by_name/{node_name}"
 get_relation_from ="https://jdm-api.demo.lirmm.fr/v0/relations/from/{node1_name}"
 get_relation_between = "https://jdm-api.demo.lirmm.fr/v0/relations/from/{node1_name}/to/{node2_name}"
-
+get_node_by_id = "https://jdm-api.demo.lirmm.fr/v0/node_by_id/{node_id}"
 
 
 def requestWrapper(url):
@@ -52,8 +54,35 @@ node2 = "calin"
 response = requests.get(get_relation_between.format(node1_name=node1, node2_name=node2))
 print(response.status_code)
 print(response.text)"""
-
-
+def create_graph(node1_data, node2_data, relation):
+    print(node1_data)
+    print(node2_data)
+    print(relation)
+    G = nx.Graph()
+    G.add_node(node1_data["id"], name=node1_data["name"])
+    G.add_node(node2_data["id"], name=node2_data["name"])
+    G.add_edge(node1_data["id"], node2_data["id"], relation=relation)
+    frontiere= [node1_data]
+    while frontiere:
+        current_node = frontiere.pop()
+        li_relation = requestWrapper(get_relation_from.format(node1_name=current_node["name"]))
+        li_relation = json.loads(li_relation)
+        for relation in li_relation["relations"]:
+            node2Id = relation["node2"]
+            #fin node 2 name
+            node2 = None
+            for node in li_relation["nodes"]:
+                if node["id"] == node2Id:
+                    node2 = node
+            if node2 is not None and abs(relation["w"])>50:
+                G.add_node(node2["id"], name=node2["name"])
+                G.add_edge(current_node["id"], node2["id"], relation=relation["id"])
+        pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, labels=nx.get_node_attributes(G, 'name'))
+    #edge_labels = nx.get_edge_attributes(G, 'relation')
+    #nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    plt.show()
+#pigeon r_agent-1 voler
 if __name__ == "__main__":
     print("Hello world")
     while True:
@@ -68,4 +97,8 @@ if __name__ == "__main__":
             print(f"node1: {node1}, node2: {node2}, relation: {relation}")
             #print node1 id
             node1_data = getNodeByName(node1)
-            print(node1_data["id"])
+            node2_data = getNodeByName(node2)
+            create_graph(node1_data, node2_data, relation)
+            
+            
+            
