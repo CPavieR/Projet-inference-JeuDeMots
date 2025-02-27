28d7ebf7812d7afcf37d1f7a5b6f0aa1b4b903a5
CPavieR <mag@cleparo.fr>
2025-02-27 17:02:41 +0100

    mise à jour bot

--- discordBot.py
+++ discordBot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from temp import callFromDiscordInduc
+from temp import callFromDiscordAll, callFromDiscordInduc
 from temp import callFromDiscordSym
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,7 +22,10 @@ async def sym(ctx, *, message_content):
     processed_content = callFromDiscordSym(message_content)
 
     # Send the processed content back to the channel
-    await ctx.send(processed_content)
+    if processed_content != "":
+        await ctx.send(processed_content)
+    else:
+        await ctx.send("Je n'ai rien trouvé")
 
 @bot.command()
 async def ind(ctx, *, message_content):
@@ -30,9 +33,24 @@ async def ind(ctx, *, message_content):
     processed_content = callFromDiscordInduc(message_content)
 
     # Send the processed content back to the channel
-    await ctx.send(processed_content)
+    if(processed_content != ""):
+        await ctx.send(processed_content)
+    else :
+        await ctx.send("Je n'ai rien trouvé")
+
+@bot.command()
+async def all(ctx, *, message_content):
+    # Capture the content of the message after the command
+    processed_content = callFromDiscordAll(message_content)
+
+    # Send the processed content back to the channel
+    if(processed_content != ""):
+        await ctx.send(processed_content)
+    else :
+        await ctx.send("Je n'ai rien trouvé")
 
 
+#callFromDiscordAll
 #read the token from the file
 file = open("token.txt", "r")
 token = file.read()
